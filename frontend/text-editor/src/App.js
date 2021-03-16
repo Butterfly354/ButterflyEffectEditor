5d5d99b850b0c2111ad09f2ab6d951b973687135
Laila Chamma'a <laylachammaa@gmail.com>
2021-03-15 23:23:12 -0400

    topMenu

--- frontend/text-editor/src/App.js
+++ frontend/text-editor/src/App.js
@@ -1,8 +1,12 @@
+import TopMenu from './components/TopMenu/TopMenu'
+import './App.css'
+
 function App() {
   return (
     <div className="App">
+      <TopMenu />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- frontend/text-editor/src/components/TopMenu/TopMenu.js
+++ frontend/text-editor/src/components/TopMenu/TopMenu.js
@@ -1,8 +1,40 @@
-import React from "react";
-import "./TopMenu.css";
+import React from 'react'
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import ActiveGroup from './ActiveGroup/ActiveGroup'
+
+import './TopMenu.css'
 
 const TopMenu = () => (
-  <div></div>
-);
+  <div>
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="#home">Butterfly Effect</Navbar.Brand>
+      <Nav className="mr-auto">
+        <NavDropdown title="File" id="collasible-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">New file</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Open file</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Save</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Download file</NavDropdown.Item>
+        </NavDropdown>
+        <NavDropdown title="Font" id="collasible-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">
+            Change Font Size
+          </NavDropdown.Item>
+        </NavDropdown>
+        <NavDropdown title="Group" id="collasible-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">
+            Create New Group
+          </NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">
+            Delete All Groups
+          </NavDropdown.Item>
+        </NavDropdown>
+        <Nav.Link href="#home">Undo History</Nav.Link>
+        <Nav.Link href="#home">Help</Nav.Link>
+      </Nav>
+      <ActiveGroup />
+    </Navbar>
+  </div>
+)
 
-export default TopMenu;
+export default TopMenu
