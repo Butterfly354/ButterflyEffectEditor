677c05af2f196f67a389975a0771cc08b109b5c2
Laila Chamma'a <laylachammaa@gmail.com>
2021-04-08 14:52:48 -0400

    cleanup

--- src/GroupContext.js
+++ src/GroupContext.js
@@ -1,4 +1,4 @@
-import React, { useState, createContext, useEffect } from 'react';
+import React, { useState, createContext } from 'react';
 import { groupDictionary } from './backend/SmartUndoManager/SmartUndoManager';
 
 export const GroupContext = createContext();
