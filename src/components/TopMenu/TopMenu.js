a72caf61992c65ecb37cd14e80a2f379021b8088
Etienne Bolduc <etienne.bolduc37@gmail.com>
2021-04-12 21:34:51 -0400

    Update TopMenu.js

--- src/components/TopMenu/TopMenu.js
+++ src/components/TopMenu/TopMenu.js
@@ -26,6 +26,7 @@ const TopMenu = ({ forceUpdate }) => {
   const [downloadShow, setDownloadShow] = useState(false);
   const [groupShow, setGroupShow] = useState(false);
   const [deleteShow, setDeleteShow] = useState(false);
+  const [newShow, setNewShow] = useState(false);
 
   let openFileButton = React.createRef();
   let fileNameInput = React.createRef();
@@ -39,7 +40,9 @@ const TopMenu = ({ forceUpdate }) => {
         </Navbar.Brand>
         <Nav className="mr-auto">
           <NavDropdown title="File" id="collasible-nav-dropdown">
-            <NavDropdown.Item>New file</NavDropdown.Item>
+            <NavDropdown.Item onClick={() => setNewShow(true)}>
+              New file
+            </NavDropdown.Item>
             <NavDropdown.Item
               onClick={() => {
                 openFileButton.current.click();
@@ -190,6 +193,62 @@ const TopMenu = ({ forceUpdate }) => {
               setDeleteShow(false);
             }}>
             Delete
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Modal
+        animation={false}
+        size="sm"
+        show={newShow}
+        onHide={() => setNewShow(false)}
+        aria-labelledby="example-modal-sizes-title-sm">
+        <Modal.Header closeButton>
+          <Modal.Title id="example-modal-sizes-title-sm">
+            New File
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Do you want to download your current file before starting new?
+        </Modal.Body>
+        <Modal.Body>
+        <InputGroup size="sm" className="mb-3">
+          <InputGroup.Prepend>
+            <InputGroup.Text id="inputGroup-sizing-sm">
+              Filename
+            </InputGroup.Text>
+          </InputGroup.Prepend>
+          <FormControl
+            aria-label="Filename"
+            aria-describedby="inputGroup-sizing-sm"
+            ref={fileNameInput}
+          />
+        </InputGroup>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="secondary"
+            onClick={() => {
+              TextEditor.myRef.current.value = '';
+              deleteAllGroups();
+              setGroupDict(groupDict);
+              forceUpdate();
+              setNewShow(false);
+            }}>
+            Delete File
+          </Button>
+          <Button
+            variant="primary"
+            onClick={() => {
+              downloadFile(
+                  TextEditor.myRef.current.value,
+                  fileNameInput.current.value
+                );
+              TextEditor.myRef.current.value = '';
+              deleteAllGroups();
+              setGroupDict(groupDict);
+              forceUpdate();
+              setNewShow(false);
+            }}>
+            Download File
           </Button>
         </Modal.Footer>
       </Modal>
